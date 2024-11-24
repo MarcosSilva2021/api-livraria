ed2d26b7ac7604c735293fb0b5ccdcccb7c96c3e
Marcosdsj <mfsilva5585@gmail.com>
2024-11-23 21:21:19 -0300

    first comit

---
+++ src/main/java/com/devm/api_livraria/entity/Autor.java
@@ -0,0 +1,4 @@
+package com.devm.api_livraria.entity;
+
+public class Autor {
+}
---
+++ src/main/java/com/devm/api_livraria/entity/Livro.java
@@ -0,0 +1,23 @@
+package com.devm.api_livraria.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_livro")
+public class Livro implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    //private String autor;
+}

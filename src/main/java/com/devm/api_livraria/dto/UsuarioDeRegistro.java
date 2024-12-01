46ee1be68e83391cf97c5fcd928d541c9eb74a7b
Marcosdsj <mfsilva5585@gmail.com>
2024-11-30 22:12:07 -0300

    add DTO

---
+++ src/main/java/com/devm/api_livraria/dto/UsuarioDeRegistro.java
@@ -0,0 +1,4 @@
+package com.devm.api_livraria.dto;
+
+public record UsuarioDeRegistro(String nome, String email, String senha) {
+}
---
+++ src/main/java/com/devm/api_livraria/dto/UsuarioToken.java
@@ -0,0 +1,4 @@
+package com.devm.api_livraria.dto;
+
+public record UsuarioToken(String token) {
+}

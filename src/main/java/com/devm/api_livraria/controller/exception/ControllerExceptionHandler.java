68bb675b726b9949f82ae5b7d01b2ab2ec33f1ab
Marcosdsj <mfsilva5585@gmail.com>
2024-12-03 15:47:29 -0300

    Criação dos Métodos para Capturar Exceções

--- src/main/java/com/devm/api_livraria/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/com/devm/api_livraria/controller/exception/ControllerExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.devm.api_livraria.controller.exception;
 
-import com.devm.api_livraria.service.exception.EntityNotFound;
+import com.devm.api_livraria.service.exception.*;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +15,46 @@ public class ControllerExceptionHandler {
     public ResponseEntity<ErrorResponse> notFound(EntityNotFound e, HttpServletRequest request){
         String error = "Not Found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(UsuarioJaExisteException.class)
+    public ResponseEntity<ErrorResponse> usuariojaExisteErro(UsuarioJaExisteException e, HttpServletRequest request){
+        String error = "Usuário já esta cadastrado no sistema";
+        HttpStatus status = HttpStatus.CONFLICT;
+        ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(UsuarioNaoExisteException.class)
+    public ResponseEntity<ErrorResponse> usuarioNaoExisteErro(UsuarioNaoExisteException e, HttpServletRequest request){
+        String error = "Usuário não existe no sistema";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(LoginInvalidoException.class)
+    public ResponseEntity<ErrorResponse> loginInvalidoErro(LoginInvalidoException e, HttpServletRequest request){
+        String error = "Login inválido ...";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(UsuarioNaoTemPermissaoException.class)
+    public ResponseEntity<ErrorResponse> usuarioNaoTemPermissaoErro(UsuarioNaoTemPermissaoException e, HttpServletRequest request){
+        String error = "Usuário não tem permissão !";
+        HttpStatus status = HttpStatus.FORBIDDEN;
+        ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(SecurityException.class)
+    public ResponseEntity<ErrorResponse> erroDeSegurancaDoToken(SecurityException e, HttpServletRequest request){
+        String error = "Token foi inválido !";
+        HttpStatus status = HttpStatus.FORBIDDEN;
         ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }

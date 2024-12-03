78639af7a8f971300b077929c89d3c9727da593a
Marcosdsj <mfsilva5585@gmail.com>
2024-12-03 11:45:04 -0300

    add usuarioLoginController - reg. de login

---
+++ src/main/java/com/devm/api_livraria/controller/UsuarioLoginController.java
@@ -0,0 +1,35 @@
+package com.devm.api_livraria.controller;
+
+import com.devm.api_livraria.dto.UsuarioDeLogin;
+import com.devm.api_livraria.dto.UsuarioDeRegistro;
+import com.devm.api_livraria.dto.UsuarioToken;
+import com.devm.api_livraria.entity.Usuario;
+import com.devm.api_livraria.service.JWTService;
+import com.devm.api_livraria.service.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/api")
+public class UsuarioLoginController {
+
+    private UsuarioService usuarioService;
+    private JWTService jwtService;
+
+    @PostMapping(value = "/registro")
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody UsuarioDeRegistro usuarioDeRegistro){
+        var usuarioParaSalvar = usuarioService.cadastrarUsuario(usuarioDeRegistro);
+        return ResponseEntity.ok(usuarioParaSalvar);
+    }
+
+    @PostMapping(value = "/login")
+    public ResponseEntity<UsuarioToken> autenticarUsuario(@RequestBody UsuarioDeLogin usuarioDeLogin){
+        var tokenDoUsuario = jwtService.autenticaUsuario(usuarioDeLogin);
+        return ResponseEntity.ok(tokenDoUsuario);
+    }
+
+
+}
